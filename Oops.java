cf425e7d0fbe6984421ed41f340fadce523857a0
Dhruv <ddrathod510@gmail.com>
2024-10-28 15:46:21 -0400

    abstract class

--- Oops.java
+++ Oops.java
@@ -1,32 +1,16 @@
-class car {
-    private String carName;
-    private int wheels;
+abstract class A {
+    abstract public void car();
+}
 
-    public void carNameSetter(String c) {
-        carName = c;
-    }
-
-    public void wheelsSetter(int w) {
-        wheels = w;
-    }
-
-    public void carNamePrinter() {
-        System.out.println(carName);
-    }
-
-    public void wheelsPrinter() {
-        System.out.println(wheels);
+class B extends A{
+    public void car(){
+        System.out.println("Drives a car!");
     }
 }
 
 public class Oops {
     public static void main(String[] args) {
-        car c1 = new car();
-        c1.carNameSetter("Tesla");
-        c1.wheelsSetter(5);
-        // c1.carName = "volvo";
-        // c1.wheels = 4;
-        c1.carNamePrinter();
-        c1.wheelsPrinter();
+        B a1 = new B();
+        a1.car();
     }
 }
